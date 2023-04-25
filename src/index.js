723f02686f9bd5d21d828d1fec36d2cfaacf2bdc
sandyfiu <sandyra17@hotmail.com>
2023-04-25 12:06:23 -0600

    Proyecto Card Validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
-console.log(validator);
+const numeroTarjeta = document.getElementById("validar")
+numeroTarjeta.addEventListener("click", (e) => obtenerNumeroTarjeta(e))
+
+function obtenerNumeroTarjeta(e){
+  e.preventDefault();
+
+  const numeroIngresado = document.getElementById("numeroTarjeta").value;
+
+  if (validator.isValid(numeroIngresado)) {
+    alert("El Número Ingresado es Válido");
+
+  } 
+  else
+  {
+    alert ("El Número Ingresado NO es válido");
+
+  }
+  const numFinal = validator.maskify(numeroIngresado);
+  alert("Número de Tarjeta: " + numFinal);
+
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    let suma = 0;
+    
+    for (let i = creditCardNumber.length - 2; i >= 0; i = i - 2) {
+      const numMult = parseInt(creditCardNumber[i]) * 2;
+
+      if (numMult >= 10) {
+        suma +=numMult - 9;
+      }
+      else
+      {
+        suma += numMult;
+      }
+
+    }
+
+    for (let i = creditCardNumber.length - 1; i >= 0; i = i -2) {
+      suma += parseInt(creditCardNumber[i]);
+    }
+
+    if (suma % 10 === 0) {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  },
+
+
+  maskify: function (creditCardNumber) {
+    let numeroMask = "";
+    const ultimosCuatroNum = creditCardNumber.slice(-4);
+
+    for (let i = 0; i < creditCardNumber.length - 4; i++) {
+      numeroMask += "*";
+    }
+    return numeroMask + ultimosCuatroNum;
+  },
 };
 
 export default validator;
